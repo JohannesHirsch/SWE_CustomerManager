4f20b0abc741a1c98703d55b0f03dc1460764ec6
JohannesHirsch <johannes.hirsch2@students.fh-wels.at>
2016-05-26 11:21:47 +0200

    struct Error added

---
+++ customerDLL/Error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customerDLL
+{
+    public struct Error
+    {
+        int Code { get; set; }
+        // Message, Wargning....
+    }
+}
